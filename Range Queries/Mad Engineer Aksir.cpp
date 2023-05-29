7da02a2e705b19c7b8c231025b7bb8d30c8188e5
Zahid Hasan Mozumder <73828619+Zahid-Hasan-Mozumder@users.noreply.github.com>
2023-05-30 01:11:52 +0600

    Create Mad Engineer Aksir.cpp

---
+++ Range Queries/Mad Engineer Aksir.cpp
@@ -0,0 +1,80 @@
+// Problem link: https://toph.co/p/mad-engineer-aksir
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#include <bits/stdc++.h>
+#include <climits>
+
+using namespace std;
+
+#define ll long long int
+
+#define pii pair<int, int>
+#define pli pair<ll, int>
+#define pil pair<int, ll>
+#define pll pair<ll, ll>
+#define psi pair<string, int>
+#define psl pair<string, ll>
+#define piii pair<pair<int, int>, int>
+#define pipii pair<int, pair<int, int>>
+#define piipii pair<pair<int, int>, pair<int, int>>
+#define vi vector<int>
+#define vll vector<ll>
+#define vb vector<bool>
+#define vvi vector<vector<int>>
+#define vvll vector<vector<ll>>
+#define vvb vector<vector<bool>>
+#define vs vector<string>
+#define vvs vector<vector<string>>
+#define vpii vector<pair<int, int>>
+#define vpll vector<pair<ll, ll>>
+#define vpli vector<pair<ll, int>>
+#define vpil vector<pair<int, ll>>
+#define vpipii vector<pair<int, pair<int, int>>>
+#define vvpii vector<vector<pair<int, int>>>
+#define vvpll vector<vector<pair<ll, ll>>>
+#define plpii pair<ll, pair<int, int>>
+#define vplpii vector<pair<ll, pair<int, int>>>
+#define vvpil vector<vector<pair<int, ll>>>
+#define pb push_back
+#define pf push_front
+#define ff first
+#define ss second
+#define vvvll vector<vector<vector<ll>>>
+
+const ll mod = 1e9+7;
+//const ll mod = 998244353;
+const int maxN = 5e6+5;
+const int sz = 1e6+2;
+const double pi = acos(-1.0);
+const double PI = 3.1415926535897;
+
+void zahid(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+
+int main(){
+
+    zahid();
+
+    int n, q; cin >> n >> q;
+    vi a(n + 2, 0);
+    while(q--){
+        int l, r; cin >> l >> r;
+        a[l]++; a[r + 1]--;
+    }
+    for(int i = 2; i <= n; i++){
+        a[i] += a[i - 1];
+    }
+    for(int i = 1; i <= n; i++){
+        if(i == 1){
+            if(a[i] & 1) cout << 1;
+            else cout << 0;    
+        }
+        else{
+            if(a[i] & 1) cout << " " << 1;
+            else cout << " " << 0;
+        }
+    }
+    cout << '\n';
+}
