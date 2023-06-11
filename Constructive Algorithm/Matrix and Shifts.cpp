33853861e698ad1e0046b5b87b3c315e008f7eec
Zahid Hasan Mozumder <73828619+Zahid-Hasan-Mozumder@users.noreply.github.com>
2023-06-11 13:04:49 +0600

    Create Matrix and Shifts.cpp

---
+++ Constructive Algorithm/Matrix and Shifts.cpp
@@ -0,0 +1,96 @@
+// Problem link: https://codeforces.com/problemset/problem/1660/E
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#include <bits/stdc++.h>
+#include <climits>
+
+using namespace std;
+
+#define ll long long int
+
+#define pii pair<int, int>
+#define pli pair<ll, int>
+#define pil pair<int, ll>
+#define pll pair<ll, ll>
+#define psi pair<string, int>
+#define psl pair<string, ll>
+#define piii pair<pair<int, int>, int>
+#define pipii pair<int, pair<int, int>>
+#define plpii pair<ll, pair<int, int>>
+#define plpll pair<ll, pair<ll, ll>>
+#define piipii pair<pair<int, int>, pair<int, int>>
+#define vi vector<int>
+#define vll vector<ll>
+#define vb vector<bool>
+#define vvi vector<vector<int>>
+#define vvll vector<vector<ll>>
+#define vvb vector<vector<bool>>
+#define vs vector<string>
+#define vvvll vector<vector<vector<ll>>>
+#define vvs vector<vector<string>>
+#define vpii vector<pair<int, int>>
+#define vpll vector<pair<ll, ll>>
+#define vpli vector<pair<ll, int>>
+#define vpil vector<pair<int, ll>>
+#define vpipii vector<pair<int, pair<int, int>>>
+#define vvpii vector<vector<pair<int, int>>>
+#define vvpll vector<vector<pair<ll, ll>>>
+#define vplpii vector<pair<ll, pair<int, int>>>
+#define vplpll vector<pair<ll, pair<ll, ll>>>
+#define vvpil vector<vector<pair<int, ll>>>
+#define pb push_back
+#define pf push_front
+#define ff first
+#define ss second
+
+const ll mod = 1e9+7;
+//const ll mod = 998244353;
+const int maxN = 5e6+5;
+const int sz = 1e6+2;
+const double pi = acos(-1.0);
+const double PI = 3.1415926535897;
+
+void zahid(){
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("Error.txt", "w", stderr);
+    #endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+
+int main(){
+
+    zahid();
+
+    int t; cin >> t;
+    while(t--){
+        string line; getline(cin, line);
+        int n; cin >> n;
+        vs a;
+        for(int i = 0; i < n; i++){
+            string s; cin >> s;
+            a.pb(s);
+        }
+        int tot = 0;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                tot += a[i][j] - '0';
+            }
+        }
+        int ans = INT_MAX;
+        for(int k = 0; k < n; k++){
+            int cnt = 0;
+            int i = 0, j = k;
+            while(i < n){
+                if(a[i][j] == '1') cnt++;
+                i++;
+                j++;
+                j %= n;
+            }
+            ans = min(ans, (n - cnt) + (tot - cnt));
+        }
+        cout << ans << '\n';
+    }
+}
