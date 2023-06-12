4d0174d8a82623ebc04b7668d68c28d0090d8ee8
Zahid Hasan Mozumder <73828619+Zahid-Hasan-Mozumder@users.noreply.github.com>
2023-06-13 02:04:50 +0600

    Create Alice and the Cake.cpp

---
+++ Constructive Algorithm/Alice and the Cake.cpp
@@ -0,0 +1,102 @@
+// Problem link: https://codeforces.com/problemset/problem/1654/C
+//---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#include <bits/stdc++.h>
+#include <climits>
+
+using namespace std;
+
+#define ll long long int
+
+#define pii pair<int, int>
+#define pli pair<ll, int>
+#define pil pair<int, ll>
+#define pll pair<ll, ll>
+#define psi pair<string, int>
+#define psl pair<string, ll>
+#define piii pair<pair<int, int>, int>
+#define pipii pair<int, pair<int, int>>
+#define plpii pair<ll, pair<int, int>>
+#define plpll pair<ll, pair<ll, ll>>
+#define piipii pair<pair<int, int>, pair<int, int>>
+#define vi vector<int>
+#define vll vector<ll>
+#define vb vector<bool>
+#define vvi vector<vector<int>>
+#define vvll vector<vector<ll>>
+#define vvb vector<vector<bool>>
+#define vs vector<string>
+#define vvvll vector<vector<vector<ll>>>
+#define vvs vector<vector<string>>
+#define vpii vector<pair<int, int>>
+#define vpll vector<pair<ll, ll>>
+#define vpli vector<pair<ll, int>>
+#define vpil vector<pair<int, ll>>
+#define vpipii vector<pair<int, pair<int, int>>>
+#define vvpii vector<vector<pair<int, int>>>
+#define vvpll vector<vector<pair<ll, ll>>>
+#define vplpii vector<pair<ll, pair<int, int>>>
+#define vplpll vector<pair<ll, pair<ll, ll>>>
+#define vvpil vector<vector<pair<int, ll>>>
+#define pb push_back
+#define pf push_front
+#define ff first
+#define ss second
+
+const ll mod = 1e9+7;
+//const ll mod = 998244353;
+const int maxN = 5e6+5;
+const int sz = 1e6+2;
+const double pi = acos(-1.0);
+const double PI = 3.1415926535897;
+
+void zahid(){
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("Error.txt", "w", stderr);
+    #endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+
+int main(){
+
+    zahid();
+
+    int t; cin >> t;
+    while(t--){
+        int n; cin >> n;
+        vll a(n);
+        ll sum = 0;
+        priority_queue<ll> pq, pq2;
+        for(int i = 0; i < n; i++){
+            cin >> a[i];
+            sum += a[i];
+            pq.push(a[i]);
+        }
+        bool flag = true;
+        pq2.push(sum);
+        while(!pq2.empty()){
+            ll cur = pq2.top();
+            pq2.pop();
+            if(pq.top() > cur){
+                flag = false;
+                break;
+            }
+            else{
+                if(pq.top() == cur){
+                    pq.pop();
+                }
+                else{
+                    ll x = cur / 2;
+                    ll y = (cur + 1) / 2;
+                    pq2.push(x);
+                    pq2.push(y);
+                }
+            }
+        }
+        if(flag) cout << "YES" << '\n';
+        else cout << "NO" << '\n';
+    }
+}
