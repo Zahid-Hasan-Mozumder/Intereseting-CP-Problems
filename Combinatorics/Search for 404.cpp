c5f8d6b5f8873951e21d585218dad79b4f3dba2f
Zahid Hasan Mozumder <73828619+Zahid-Hasan-Mozumder@users.noreply.github.com>
2023-06-15 14:06:00 +0600

    Create Search for 404.cpp

---
+++ Combinatorics/Search for 404.cpp
@@ -0,0 +1,119 @@
+// Problem link: https://www.codechef.com/problems/CS2023_404
+//---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#include <bits/stdc++.h>
+#include <climits>
+
+using namespace std;
+
+#define ll long long int
+
+#define pii pair<int, int>
+#define pli pair<ll, int>
+#define pil pair<int, ll>
+#define pll pair<ll, ll>
+#define psi pair<string, int>
+#define psl pair<string, ll>
+#define piii pair<pair<int, int>, int>
+#define pipii pair<int, pair<int, int>>
+#define plpii pair<ll, pair<int, int>>
+#define plpll pair<ll, pair<ll, ll>>
+#define piipii pair<pair<int, int>, pair<int, int>>
+#define vi vector<int>
+#define vll vector<ll>
+#define vb vector<bool>
+#define vvi vector<vector<int>>
+#define vvll vector<vector<ll>>
+#define vvb vector<vector<bool>>
+#define vs vector<string>
+#define vvvll vector<vector<vector<ll>>>
+#define vvs vector<vector<string>>
+#define vpii vector<pair<int, int>>
+#define vpll vector<pair<ll, ll>>
+#define vpli vector<pair<ll, int>>
+#define vpil vector<pair<int, ll>>
+#define vpipii vector<pair<int, pair<int, int>>>
+#define vvpii vector<vector<pair<int, int>>>
+#define vvpll vector<vector<pair<ll, ll>>>
+#define vplpii vector<pair<ll, pair<int, int>>>
+#define vplpll vector<pair<ll, pair<ll, ll>>>
+#define vvpil vector<vector<pair<int, ll>>>
+#define pb push_back
+#define pf push_front
+#define ff first
+#define ss second
+
+const ll mod = 1e9+7;
+//const ll mod = 998244353;
+const int maxN = 5e6+5;
+const int sz = 1e6+2;
+const double pi = acos(-1.0);
+const double PI = 3.1415926535897;
+
+vector<ll> POW(sz);
+void generate(){
+    POW[0] = 1;
+    for(int i = 1; i < sz; i++){
+        POW[i] = POW[i - 1] * 2ll;
+        POW[i] %= mod;
+    }
+}
+
+void zahid(){
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("Error.txt", "w", stderr);
+    #endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+
+int main(){
+
+    zahid();
+    generate();
+
+    int t; cin >> t;
+    while(t--){
+        int n; cin >> n;
+        string s; cin >> s;
+        vll pre_star(n, 0), pre_four(n, 0), post_star(n, 0), post_four(n, 0);
+        ll star = 0, four = 0;
+        for(int i = 0; i < n; i++){
+            pre_star[i] = star;
+            pre_four[i] = four;
+            if(s[i] == '*') star++;
+            if(s[i] == '4') four++;
+        }
+        star = 0, four = 0;
+        for(int i = n - 1; i >= 0; i--){
+            post_star[i] = star;
+            post_four[i] = four;
+            if(s[i] == '*') star++;
+            if(s[i] == '4') four++;
+        }
+        ll ans = 0;
+        for(int i = 0; i < n; i++){
+            if(s[i] == '0' || s[i] == '*'){
+                ll fixed = POW[pre_star[i]] * pre_four[i];
+                fixed %= mod;
+                ll comb = POW[pre_star[i] - 1] * pre_star[i];
+                comb %= mod;
+                ll left = fixed + comb;
+                left %= mod;
+
+                fixed = POW[post_star[i]] * post_four[i];
+                fixed %= mod;
+                comb = POW[post_star[i] - 1] * post_star[i];
+                comb %= mod;
+                ll right = fixed + comb;
+                right %= mod;
+
+                ans += (left * right);
+                ans %= mod;
+            }
+        }
+        cout << ans << '\n';
+    }
+}
