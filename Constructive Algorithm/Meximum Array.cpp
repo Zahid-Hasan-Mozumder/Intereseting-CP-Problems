db2d623c39949a9fc7f120ba15da3838fb806762
Zahid Hasan Mozumder <73828619+Zahid-Hasan-Mozumder@users.noreply.github.com>
2023-06-17 21:35:38 +0600

    Create Meximum Array.cpp

---
+++ Constructive Algorithm/Meximum Array.cpp
@@ -0,0 +1,94 @@
+// Problem link: https://codeforces.com/problemset/problem/1628/A
+// Video editorial: https://www.youtube.com/watch?v=6gmM4yAZNfM&t=1235s
+//---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#include <bits/stdc++.h>
+#include <climits>
+
+using namespace std;
+
+#define ll long long int
+
+#define pii pair<int, int>
+#define pli pair<ll, int>
+#define pil pair<int, ll>
+#define pll pair<ll, ll>
+#define psi pair<string, int>
+#define psl pair<string, ll>
+#define piii pair<pair<int, int>, int>
+#define pipii pair<int, pair<int, int>>
+#define plpii pair<ll, pair<int, int>>
+#define plpll pair<ll, pair<ll, ll>>
+#define piipii pair<pair<int, int>, pair<int, int>>
+#define vi vector<int>
+#define vll vector<ll>
+#define vb vector<bool>
+#define vvi vector<vector<int>>
+#define vvll vector<vector<ll>>
+#define vvb vector<vector<bool>>
+#define vs vector<string>
+#define vvvll vector<vector<vector<ll>>>
+#define vvs vector<vector<string>>
+#define vpii vector<pair<int, int>>
+#define vpll vector<pair<ll, ll>>
+#define vpli vector<pair<ll, int>>
+#define vpil vector<pair<int, ll>>
+#define vpipii vector<pair<int, pair<int, int>>>
+#define vvpii vector<vector<pair<int, int>>>
+#define vvpll vector<vector<pair<ll, ll>>>
+#define vplpii vector<pair<ll, pair<int, int>>>
+#define vplpll vector<pair<ll, pair<ll, ll>>>
+#define vvpil vector<vector<pair<int, ll>>>
+#define pb push_back
+#define pf push_front
+#define ff first
+#define ss second
+
+const ll mod = 1e9+7;
+//const ll mod = 998244353;
+const int N = 2e5 + 5;
+const int sz = 1e6+2;
+const double pi = acos(-1.0);
+const double PI = 3.1415926535897;
+
+void zahid(){
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("Error.txt", "w", stderr);
+    #endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+
+int main(){
+
+    zahid();
+
+    int t; cin >> t;
+    while(t--){
+        int n; cin >> n;
+        vi a(n), b;
+        map<int, queue<int>> ind;
+        for(int i = 0; i < n; i++){
+            cin >> a[i];
+            ind[a[i]].push(i);
+        }
+        int mxInd = 0;
+        while(mxInd < n){
+            int mex = 0;
+            int currMax = mxInd;
+            while(ind[mex].size()){
+                currMax = max(currMax, ind[mex].front());
+                mex++;
+            }
+            mxInd = currMax + 1;
+            b.pb(mex);
+            for(int i = 0; i < mex; i++){
+                while(ind[i].size() && ind[i].front() <= currMax) ind[i].pop();
+            }
+        }
+        cout << b.size() << '\n';
+        for(int i = 0; i < b.size(); i++) cout << b[i] << " "; cout << '\n';
+    }
+}
