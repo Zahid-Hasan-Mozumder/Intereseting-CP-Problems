e79cd891168471fc44e6709ce42b00ae753b208e
Zahid Hasan Mozumder <73828619+Zahid-Hasan-Mozumder@users.noreply.github.com>
2023-06-22 15:15:14 +0600

    Create Palindromes Coloring.cpp

---
+++ String/Palindromes Coloring.cpp
@@ -0,0 +1,89 @@
+// Problem link: https://codeforces.com/problemset/problem/1624/D
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#include <bits/stdc++.h>
+#include <climits>
+
+using namespace std;
+
+#define ll long long int
+
+#define pii pair<int, int>
+#define pli pair<ll, int>
+#define pil pair<int, ll>
+#define pll pair<ll, ll>
+#define psi pair<string, int>
+#define psl pair<string, ll>
+#define piii pair<pair<int, int>, int>
+#define pipii pair<int, pair<int, int>>
+#define plpii pair<ll, pair<int, int>>
+#define plpll pair<ll, pair<ll, ll>>
+#define piipii pair<pair<int, int>, pair<int, int>>
+#define vi vector<int>
+#define vll vector<ll>
+#define vb vector<bool>
+#define vvi vector<vector<int>>
+#define vvll vector<vector<ll>>
+#define vvb vector<vector<bool>>
+#define vs vector<string>
+#define vvvll vector<vector<vector<ll>>>
+#define vvs vector<vector<string>>
+#define vpii vector<pair<int, int>>
+#define vpll vector<pair<ll, ll>>
+#define vpli vector<pair<ll, int>>
+#define vpil vector<pair<int, ll>>
+#define vpipii vector<pair<int, pair<int, int>>>
+#define vvpii vector<vector<pair<int, int>>>
+#define vvpll vector<vector<pair<ll, ll>>>
+#define vplpii vector<pair<ll, pair<int, int>>>
+#define vplpll vector<pair<ll, pair<ll, ll>>>
+#define vvpil vector<vector<pair<int, ll>>>
+#define pb push_back
+#define pf push_front
+#define ff first
+#define ss second
+
+const ll mod = 1e9+7;
+//const ll mod = 998244353;
+const int N = 2e5 + 5;
+const int sz = 1e6+2;
+const double pi = acos(-1.0);
+const double PI = 3.1415926535897;
+
+void zahid(){
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("Error.txt", "w", stderr);
+    #endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+
+int main(){
+
+    zahid();
+
+    int t; cin >> t;
+    while(t--){
+        int n, k; cin >> n >> k;
+        string s; cin >> s;
+        map<char, int> mp;
+        for(int i = 0; i < n; i++){
+            mp[s[i]]++;
+        }
+        int duo = 0, solo = 0;
+        for(auto it : mp){
+            duo += it.ss / 2;
+            solo += it.ss % 2;
+        }
+        int per_seg = duo / k;
+        int mnLen = per_seg * 2;
+        if(per_seg == 0) cout << 1 << '\n';
+        else{
+            int mnSeg = min(k, duo / per_seg);
+            if(mnSeg <= (max(0, duo - (mnSeg * per_seg)) * 2) + solo) mnLen++;
+            cout << mnLen << '\n';
+        }
+    }
+}
