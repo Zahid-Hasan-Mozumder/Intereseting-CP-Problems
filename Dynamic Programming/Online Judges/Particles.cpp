30266eac6daf8cfcd817d98ea0468d2fb69239ca
Zahid Hasan Mozumder <73828619+Zahid-Hasan-Mozumder@users.noreply.github.com>
2023-07-11 23:59:54 +0600

    Create Particles.cpp

---
+++ Dynamic Programming/Online Judges/Particles.cpp
@@ -0,0 +1,97 @@
+// Problem link: https://codeforces.com/contest/1844/problem/C
+//---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#include <bits/stdc++.h>
+#include <climits>
+
+using namespace std;
+
+#define ll long long int
+
+#define pii pair<int, int>
+#define pli pair<ll, int>
+#define pil pair<int, ll>
+#define pll pair<ll, ll>
+#define psi pair<string, int>
+#define psl pair<string, ll>
+#define piii pair<pair<int, int>, int>
+#define pipii pair<int, pair<int, int>>
+#define pipll pair<int, pair<ll, ll>>
+#define plpii pair<ll, pair<int, int>>
+#define plpll pair<ll, pair<ll, ll>>
+#define piipii pair<pair<int, int>, pair<int, int>>
+#define vi vector<int>
+#define vll vector<ll>
+#define vb vector<bool>
+#define vvi vector<vector<int>>
+#define vvll vector<vector<ll>>
+#define vvb vector<vector<bool>>
+#define vs vector<string>
+#define vvvll vector<vector<vector<ll>>>
+#define vvs vector<vector<string>>
+#define vpii vector<pair<int, int>>
+#define vpll vector<pair<ll, ll>>
+#define vpli vector<pair<ll, int>>
+#define vpil vector<pair<int, ll>>
+#define vpipii vector<pair<int, pair<int, int>>>
+#define vpipll vector<pair<int, pair<ll, ll>>>
+#define vvpii vector<vector<pair<int, int>>>
+#define vvpll vector<vector<pair<ll, ll>>>
+#define vplpii vector<pair<ll, pair<int, int>>>
+#define vplpll vector<pair<ll, pair<ll, ll>>>
+#define vvpil vector<vector<pair<int, ll>>>
+#define pb push_back
+#define pf push_front
+#define ff first
+#define ss second
+
+const ll mod = 1e9 + 7;
+//const ll mod = 998244353;
+const int N = 2e6 + 2;
+const int sz = 1e6 + 2;
+const double pi = acos(-1.0);
+const double PI = 3.1415926535897;
+const double eps = 1e-6;
+
+ll n; 
+vll a, dp;
+
+ll depression(ll i){
+    if(i == n - 1) return a[i];
+    if(dp[i] != -1) return dp[i];
+    ll res = LLONG_MIN;
+    res = max(res, a[i]);
+    if(i + 2 < n) res = max(res, a[i] + depression(i + 2));
+    if(i + 2 < n) res = max(res, depression(i + 2));
+    return dp[i] = res;
+}
+
+void zahid(){
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("Error.txt", "w", stderr);
+    #endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+
+int main(){
+
+    zahid();
+
+    int t; cin >> t;
+    while(t--){
+        cin >> n;
+        a.clear(); a.resize(n);
+        dp.clear(); dp.resize(n, -1);
+        for(int i = 0; i < n; i++){
+            cin >> a[i];
+        }
+        if(n == 1) cout << a[0] << '\n';
+        else{
+            ll ans = max(depression(0), depression(1));
+            cout << ans << '\n';     
+        }
+    }
+}
