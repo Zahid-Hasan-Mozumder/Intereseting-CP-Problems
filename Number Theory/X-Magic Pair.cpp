aa6b49c88669f64ac20b9e409c75c46163aba3e2
Zahid Hasan Mozumder <73828619+Zahid-Hasan-Mozumder@users.noreply.github.com>
2023-08-01 23:48:37 +0600

    Create X-Magic Pair.cpp

---
+++ Number Theory/X-Magic Pair.cpp
@@ -0,0 +1,93 @@
+// Problem link: https://codeforces.com/contest/1612/problem/D
+// Idea of GCD
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#include <bits/stdc++.h>
+#include <climits>
+
+using namespace std;
+
+#define ll long long int
+
+#define pii pair<int, int>
+#define pli pair<ll, int>
+#define pil pair<int, ll>
+#define pll pair<ll, ll>
+#define psi pair<string, int>
+#define psl pair<string, ll>
+#define piii pair<pair<int, int>, int>
+#define pipii pair<int, pair<int, int>>
+#define pipll pair<int, pair<ll, ll>>
+#define plpii pair<ll, pair<int, int>>
+#define plpll pair<ll, pair<ll, ll>>
+#define piipii pair<pair<int, int>, pair<int, int>>
+#define vi vector<int>
+#define vll vector<ll>
+#define vb vector<bool>
+#define vvi vector<vector<int>>
+#define vvll vector<vector<ll>>
+#define vvb vector<vector<bool>>
+#define vs vector<string>
+#define vvvll vector<vector<vector<ll>>>
+#define vvs vector<vector<string>>
+#define vpii vector<pair<int, int>>
+#define vpll vector<pair<ll, ll>>
+#define vpli vector<pair<ll, int>>
+#define vpil vector<pair<int, ll>>
+#define vpipii vector<pair<int, pair<int, int>>>
+#define vpipll vector<pair<int, pair<ll, ll>>>
+#define vvpii vector<vector<pair<int, int>>>
+#define vvpll vector<vector<pair<ll, ll>>>
+#define vplpii vector<pair<ll, pair<int, int>>>
+#define vplpll vector<pair<ll, pair<ll, ll>>>
+#define vvpil vector<vector<pair<int, ll>>>
+#define pb push_back
+#define pf push_front
+#define ff first
+#define ss second
+
+const ll mod = 1e9 + 7;
+//const ll mod = 998244353;
+const double pi = acos(-1.0);
+const double PI = 3.1415926535897;
+const double eps = 1e-6;
+
+ll a, b, x;
+
+bool check(ll a, ll b){
+    if(x == 0) return true;
+    if(a == x || b == x) return true;
+    ll mn = min(a, b), mx = max(a, b);
+    if(mn == 0 || x > mx) return false;
+    if(x > mn){
+        if((x - mn) % mn == mx % mn) return true;
+        else return false;
+    }
+    else{
+        bool res = false;
+        res |= check(mx % mn, mn);
+        return res;
+    }
+}
+
+void zahid(){
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("Error.txt", "w", stderr);
+    #endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+
+int main(){
+
+    zahid();
+
+    int t; cin >> t;
+    while(t--){
+        cin >> a >> b >> x;
+        bool ans = check(a, b);
+        (ans) ? puts("YES") : puts("NO");
+    }
+}
