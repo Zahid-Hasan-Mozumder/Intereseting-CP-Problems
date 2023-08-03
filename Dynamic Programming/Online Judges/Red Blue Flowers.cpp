9ba9ae80283ee44e29135df6921892024ebf034d
Zahid Hasan Mozumder <73828619+Zahid-Hasan-Mozumder@users.noreply.github.com>
2023-08-03 14:39:24 +0600

    Create Red Blue Flowers.cpp

---
+++ Dynamic Programming/Online Judges/Red Blue Flowers.cpp
@@ -0,0 +1,93 @@
+// Problem link: https://www.codechef.com/problems/RBFLOWERS?tab=statement
+//---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#include <bits/stdc++.h>
+#include <climits>
+
+using namespace std;
+
+#define ll long long int
+
+#define pii pair<int, int>
+#define pli pair<ll, int>
+#define pil pair<int, ll>
+#define pll pair<ll, ll>
+#define psi pair<string, int>
+#define psl pair<string, ll>
+#define piii pair<pair<int, int>, int>
+#define pipii pair<int, pair<int, int>>
+#define pipll pair<int, pair<ll, ll>>
+#define plpii pair<ll, pair<int, int>>
+#define plpll pair<ll, pair<ll, ll>>
+#define piipii pair<pair<int, int>, pair<int, int>>
+#define vi vector<int>
+#define vll vector<ll>
+#define vb vector<bool>
+#define vvi vector<vector<int>>
+#define vvll vector<vector<ll>>
+#define vvb vector<vector<bool>>
+#define vs vector<string>
+#define vvvi vector<vector<vector<int>>>
+#define vvvll vector<vector<vector<ll>>>
+#define vvs vector<vector<string>>
+#define vpii vector<pair<int, int>>
+#define vpll vector<pair<ll, ll>>
+#define vpli vector<pair<ll, int>>
+#define vpil vector<pair<int, ll>>
+#define vpipii vector<pair<int, pair<int, int>>>
+#define vpipll vector<pair<int, pair<ll, ll>>>
+#define vvpii vector<vector<pair<int, int>>>
+#define vvpll vector<vector<pair<ll, ll>>>
+#define vplpii vector<pair<ll, pair<int, int>>>
+#define vplpll vector<pair<ll, pair<ll, ll>>>
+#define vvpil vector<vector<pair<int, ll>>>
+#define pb push_back
+#define pf push_front
+#define ff first
+#define ss second
+
+const ll mod = 1e9 + 7;
+//const ll mod = 998244353;
+const double pi = acos(-1.0);
+const double PI = 3.1415926535897;
+const double eps = 1e-6;
+
+int n;
+vi r, b;
+int dp[105][20005];
+
+int depression(int i, int red, int blue){
+    if(i == n) return min(red, blue);
+    if(dp[i][red] >= blue) return 0;
+    dp[i][red] = blue;
+    int res = INT_MIN;
+    res = max(res, depression(i + 1, red + r[i], blue));
+    res = max(res, depression(i + 1, red, blue + b[i]));
+    return res;
+}
+
+void zahid(){
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("Error.txt", "w", stderr);
+    #endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+
+int main(){
+
+    zahid();
+
+    int t; cin >> t;
+    while(t--){
+        cin >> n;
+        r.clear(); r.resize(n);
+        b.clear(); b.resize(n);
+        for(int i = 0; i < n; i++) cin >> r[i];
+        for(int i = 0; i < n; i++) cin >> b[i];
+        memset(dp, -1, sizeof(dp));
+        cout << depression(0, 0, 0) << '\n';
+    }
+}
